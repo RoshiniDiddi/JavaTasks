0462396b30c6212c4c15def8149ab696c9408725
rodi1022 <roshini.diddi@netcracker.com>
2022-11-15 12:28:00 +0530

    Java Tasks

--- src/Main.java
+++ src/Main.java
@@ -44,10 +44,12 @@ public class Main {
         for(int i=0;i<n;i++){
             System.out.println(arr[i]);
         }
-        /*Task 3. Looking for prime numbers
+        /*
+        Task 3. Looking for prime numbers
         Implement a logic that looks for all prime numbers on the specified interval.
         Use interval [2, 100] to test it (2 and 100 are included into the interval).
-        Print the test result.*/
+        Print the test result.
+        */
         System.out.println("Task 3 :");
         System.out.println("Prime numbers in the interval[2,100]");
         for(int i=2;i<=100;i++){
@@ -62,14 +64,18 @@ public class Main {
             if(flag==true){System.out.print(i+"  ");}
         }
         System.out.println();
-        /* Task 4 testing*/
+        /*
+         Task 4 testing
+         */
         System.out.println("Task 4 :");
         int number=5;
         int[] array={2,5,7,4,5,2,3,8,6,1,3,5,4,5};
         System.out.println("Array of integers:"+Arrays.toString(array));
         System.out.println(Arrays.toString(removeNumber(number,array)));
 
-        //Task 5: testing
+        /*
+        Task 5: testing
+        */
         System.out.println("Task 5 :");
         countLetters("netcrackertechnologies").forEach((k,v)-> System.out.println( k +" : " + v));
 
@@ -87,24 +93,27 @@ public class Main {
             }
         }
         System.out.println();
-        //Task 7 Testing
+        /*
+        Task 7 Testing
+        */
         System.out.println("Task 7 :");
         System.out.print("Unique letter from String \"netcrackertechnologies\" "+unique("netcrackertechnologies"));
     }
 
-    /* Task 4. Removal of the number
-        Implement a method that receives an integer number and an array of integers.
-        Method should remove every element of an array that contains the number from the first argument.
-        There should be no gaps in the result array.
-        Print the test result.*/
+    /*
+    Task 4. Removal of the number
+    Implement a method that receives an integer number and an array of integers.
+    Method should remove every element of an array that contains the number from the first argument.
+    There should be no gaps in the result array.
+    Print the test result.*/
     public static int[] removeNumber(int num,int[] array){
         return Arrays.stream(array).filter(value -> value!=num).toArray();
     }
     /*
-        Task:5. Count letters
-        Implement a method that receives a string and returns a map with letters from this string
-        and a number of how many times this letter occur in the string.
-        Print the test result.*/
+    Task:5. Count letters
+    Implement a method that receives a string and returns a map with letters from this string
+    and a number of how many times this letter occur in the string.
+    Print the test result.*/
     public static Map countLetters(String str){
         Map<Character, Integer> result=new HashMap<>();
         for(char ch:str.toCharArray()){
